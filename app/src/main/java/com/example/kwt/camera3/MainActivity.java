97409c8da1579365071cbd996ad902bdd9ccb8bf
Omar <o.alghaith@hotmail.com>
2019-01-20 22:15:05 +1300

    Canny + Grey Working

--- app/src/main/java/com/example/kwt/camera3/MainActivity.java
+++ app/src/main/java/com/example/kwt/camera3/MainActivity.java
@@ -1,27 +1,54 @@
 package com.example.kwt.camera3;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.graphics.Bitmap;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.util.Log;
+import android.view.Menu;
+import android.view.SurfaceView;
+import android.view.WindowManager;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import org.opencv.android.BaseLoaderCallback;
 import org.opencv.android.CameraBridgeViewBase;
-import org.opencv.android.JavaCameraView;
+import org.opencv.android.CameraBridgeViewBase.CvCameraViewFrame;
+import org.opencv.android.CameraBridgeViewBase.CvCameraViewListener2;
+import org.opencv.android.LoaderCallbackInterface;
 import org.opencv.android.OpenCVLoader;
-import org.opencv.core.Core;
+import org.opencv.android.Utils;
 import org.opencv.core.CvType;
 import org.opencv.core.Mat;
 import org.opencv.imgproc.Imgproc;
 
-public class MainActivity extends AppCompatActivity implements CameraBridgeViewBase.CvCameraViewListener2 {
 
-    CameraBridgeViewBase cameraBridgeViewBase;
+public class MainActivity extends Activity implements CvCameraViewListener2 {
+    private static final String TAG = "MyActivity_OpenCVTest";
+    private CameraBridgeViewBase mOpenCvCameraView;
+    ImageView imageview;
+    ImageView imageView_2;//new
+    Bitmap grayBitmap;
+    Bitmap cannyBitmap;//new
+    Mat mRgba;
+    Mat gray;
+    Mat canny;//new
 
-    Mat mat1,mat2,mat3;
-    Mat Gray;
-    BaseLoaderCallback baseLoaderCallback;
-
+    //OpenCV Initialization
+    private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
+        public void onManagerConnected(int status) {
+            switch (status) {
+                case LoaderCallbackInterface.SUCCESS: {
+                    Log.i(TAG, "OpenCV loaded Successfully");
+                    mOpenCvCameraView.enableView();
+                }
+                break;
+                default: {
+                    super.onManagerConnected(status);
+                }
+                break;
+            }
+        }
+    };
 
     // Used to load the 'native-lib' library on application startup.
     static {
@@ -32,60 +59,23 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.i(TAG, "called onCreate");
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        mOpenCvCameraView = findViewById(R.id.myCameraView);
+        mOpenCvCameraView.setVisibility(SurfaceView.VISIBLE);
 
-        cameraBridgeViewBase = (JavaCameraView)findViewById(R.id.myCameraView);
-        cameraBridgeViewBase.setVisibility(JavaCameraView.VISIBLE);
-        cameraBridgeViewBase.setCvCameraViewListener(this);
+        mOpenCvCameraView.setCvCameraViewListener(this);
 
-        baseLoaderCallback = new BaseLoaderCallback(this) {
-            @Override
-            public void onManagerConnected(int status) {
-                switch (status)
-                {
-                    case BaseLoaderCallback.SUCCESS:
-                        cameraBridgeViewBase.enableView();
-                        break;
-                    default:
-                        super.onManagerConnected(status);
-                        break;
-                }
-            }
-        };
-
-
-    }
-
-
-    @Override
-    public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
-
-        mat1 = inputFrame.rgba();
-        Mat mRgba = mat1.t();
-        Core.flip(mat1.t(),mRgba,1);
-        Imgproc.resize(mRgba,mRgba,mat1.size());
-        Imgproc.cvtColor(mRgba,Gray,Imgproc.COLOR_RGB2GRAY);
-        return null;
-    }
-
-    @Override
-    public void onCameraViewStopped() {
-        mat1.release();
-    }
-
-    @Override
-    public void onCameraViewStarted(int width, int height) {
-        mat1 = new Mat(width,height,CvType.CV_8UC4);
-        mat2 = new Mat(width,height,CvType.CV_8UC4);//remove
-        mat3 = new Mat(width,height,CvType.CV_8UC4);//remove
-        Gray = new Mat(width,height,CvType.CV_8SC1);
+        imageview = findViewById(R.id.imageView);
+        imageView_2 = findViewById(R.id.imageView2);//new
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        if(cameraBridgeViewBase!=null)
+        if(mOpenCvCameraView!=null)
         {
-            cameraBridgeViewBase.disableView();
+            mOpenCvCameraView.disableView();
         }
     }
 
@@ -93,14 +83,13 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
     @Override
     protected void onResume() {
         super.onResume();
-        super.onResume();
         if(!OpenCVLoader.initDebug())
         {
             Toast.makeText(getApplicationContext(),"openCV did not Loaded successfully",Toast.LENGTH_SHORT).show();
         }
         else
         {
-            baseLoaderCallback.onManagerConnected(BaseLoaderCallback.SUCCESS);
+            mLoaderCallback.onManagerConnected(BaseLoaderCallback.SUCCESS);
         }
     }
 
@@ -108,10 +97,55 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        if(cameraBridgeViewBase!=null)
+        if(mOpenCvCameraView!=null)
         {
-            cameraBridgeViewBase.disableView();
+            mOpenCvCameraView.disableView();
         }
+    }
+
+
+    public void onCameraViewStarted(int width, int height) {
+        grayBitmap = Bitmap.createBitmap(width,height,Bitmap.Config.RGB_565);
+        cannyBitmap = Bitmap.createBitmap(width,height,Bitmap.Config.RGB_565);//new
+        mRgba = new Mat(width,height,CvType.CV_8UC4);
+        gray = new Mat(width,height,CvType.CV_8SC1);
+        canny = new Mat(width,height,CvType.CV_8SC1);//new
+
+    }
+
+    public void onCameraViewStopped() {
+
+    }
+
+    public Mat onCameraFrame(CvCameraViewFrame inputFrame) {
+        //Log.i(TAG, "Got New Frame!");
+        mRgba = inputFrame.rgba();
+
+        Imgproc.cvtColor(mRgba, gray , Imgproc.COLOR_RGB2GRAY);
+        Imgproc.Canny(gray, canny, 50, 150);//new
+
+        Utils.matToBitmap(gray, grayBitmap);
+        Utils.matToBitmap(canny,cannyBitmap);//new
+
+        runOnUiThread(new Runnable() {
+
+            @Override
+            public void run() {
+
+                imageview.setImageBitmap(grayBitmap);
+                imageView_2.setImageBitmap(cannyBitmap);//new
+
+            }
+        });
+
+
+        return mRgba;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is ...
+        return false;
     }
 
     /**
--- openCVLibrary320/build/generated/source/aidl/debug/org/opencv/engine/OpenCVEngineInterface.java
+++ openCVLibrary320/build/generated/source/aidl/debug/org/opencv/engine/OpenCVEngineInterface.java
@@ -1,6 +1,6 @@
 /*
  * This file is auto-generated.  DO NOT MODIFY.
- * Original file: C:\\Users\\kwt\\Desktop\\omaaaaaaaar\\camera3\\openCVLibrary320\\src\\main\\aidl\\org\\opencv\\engine\\OpenCVEngineInterface.aidl
+ * Original file: C:\\Users\\kwt\\Desktop\\canny\\camera3\\openCVLibrary320\\src\\main\\aidl\\org\\opencv\\engine\\OpenCVEngineInterface.aidl
  */
 package org.opencv.engine;
 /**
